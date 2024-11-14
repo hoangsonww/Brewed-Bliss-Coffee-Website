4688343c65040f073424c5292af104f534841dcb
Son Nguyen <hoangson091104@gmail.com>
2024-11-14 13:33:08 -0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+	const navLinks = document.getElementById('mobile-nav-links');
+	const icon = document.getElementById('menu-icon').querySelector('i');
+
+	if (navLinks.style.height === '0px' || !navLinks.style.height) {
+		navLinks.style.height = navLinks.scrollHeight + 'px';
+		icon.classList.remove('fa-bars');
+		icon.classList.add('fa-times'); // Change to 'X' icon
+	} else {
+		navLinks.style.height = '0px';
+		icon.classList.remove('fa-times');
+		icon.classList.add('fa-bars'); // Revert back to hamburger icon
+	}
+});
