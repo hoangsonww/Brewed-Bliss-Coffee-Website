2f8683c91ffe5844ce0deb9eb89ad2ecbb603c35
Son Nguyen <hoangson091104@gmail.com>
2024-11-15 22:19:33 -0500

    Improve mobile navigation toggle functionality: update icon change logic and add comments for clarity

--- js/script.js
+++ js/script.js
@@ -8,16 +8,18 @@ document.getElementById('menu-icon').addEventListener( 'click', function () {
 	const icon = document.getElementById('menu-icon').querySelector('i');
 
 	// If the height of the mobile navigation links is 0px or not set, then set it to the scroll height of the links
-	// to open the links
-	if (navLinks.style.height === '0px' || !navLinks.style.height) {
+	if ( navLinks.style.height === '0px' || !navLinks.style.height ) {
+		// Set the height of the mobile navigation links to the scroll height of the links to push other elements down
+		// without covering them
 		navLinks.style.height = navLinks.scrollHeight + 'px';
+		// Change the icon from hamburger to close icon
 		icon.classList.remove('fa-bars');
 		icon.classList.add('fa-times', 'icon-toggle');
 	} else {
 		// If the height of the mobile navigation links is not 0px, then set it to 0px to close the links
 		navLinks.style.height = '0px';
+		// Change the icon from close to hamburger icon
 		icon.classList.remove('fa-times');
 		icon.classList.add('fa-bars', 'icon-toggle'); 
 	}
 });
-	
